68209b1c66be5dab64a9b44015b17c71341f8448
wongprom <>
2024-04-10 21:52:07 +0200

    Add Labels and Buttons with grid().

--- main.py
+++ main.py
@@ -20,10 +20,23 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+header_text = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
+header_text.grid(column=2, row=1)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=2, row=2)
+
+button_start = Button(text="Start", highlightbackground=YELLOW)
+button_start.grid(column=1, row=3)
+
+check = Label(text=" ✔", fg=GREEN, bg=YELLOW)
+check.grid(column=2, row=4)
+
+button_reset = Button(text="Reset", highlightbackground=YELLOW)
+button_reset.grid(column=3, row=3)
+
 
 window.mainloop()
