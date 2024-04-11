e8bd37ef52b0fc116fb3ea25c1daa1cb0bad9808
wongprom <>
2024-04-11 14:22:53 +0200

    Display count down on canvas.

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    print(count)
-    window.after(1000, count_down, count - 1)
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-count_down(5)
 def something(a,b,c):
     print(a)
     print(b)
@@ -36,8 +36,9 @@ header_text.grid(column=2, row=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=2, row=2)
+count_down(5)
 
 button_start = Button(text="Start", highlightbackground=YELLOW)
 button_start.grid(column=1, row=3)
